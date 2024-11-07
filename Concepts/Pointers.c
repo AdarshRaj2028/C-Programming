318cde5e8e07555fbf34788decacb457de5cc053
BasicHTML-Webpage <adarsh787011@gmail.com>
2024-11-07 13:31:41 +0530

    Added example code and concepts of pointers

--- Concepts/Pointers.c
+++ Concepts/Pointers.c
@@ -8,5 +8,30 @@ Pointers in C programming can be declared using *(asterisk symbol).
 -> The address of the operator & returns the address of a variable
 -> * is the dereference operator(Also known as indirection operator) used to get the value at a given address.
 
+%p is used to print a pointer value (an address). It outputs the address in a platform-dependent format, usually hexadecimal.
+%x is used to print an unsigned integer in hexadecimal format.
 
+-> A pointed that is not assigned any value but NULL is known as the NULL pointer.
+-> Also known as a pointer that does not point to any object of function.
+int *ptr = NULL
+
+Uses of Pointer:
+
+-> Dynamic memory allocation
+-> Arryas, Functions, and Structures
+-> Retur multiple values from a function
+-> Pointer reduces the code and improves the performanceff
 */
+
+#include <stdio.h>
+
+int main(){
+    int a = 35;
+    int *ptra = &a;
+    printf("The value stored in a is %d\n", a);    
+    printf("The value stored in a is %d\n", *ptra);    
+    printf("The address of a is %x\n", ptra);
+    printf("The address of pointer a is %p\n", ptra);
+    printf("The address of pointer a is %x", ptra);
+    return 0;
+}
