29f30a730b00184bd77f47610ddc7f180ffa37fe
AdarshRaj <adarsh787011@gmail.com>
2024-11-12 22:55:51 +0530

    Added one more small point about double

--- Concepts/sizeofdatatypes.c
+++ Concepts/sizeofdatatypes.c
@@ -3,8 +3,9 @@
 int main()
 {
   printf("%lu", sizeof(int));
-//sizeof() operator is used  to find the size of a variable or data type in bytes.
+// sizeof() operator is used  to find the size of a variable or data type in bytes.
 // The %lu format specifier is used for unsigned long integers, which is suitable for printing sizes in bytes.
+// Use %f or %lf for printing a double value
   return 0;
 }
 // It gives answer in byte, just put the data type and it will tell you its size in bytes, According to your system architecture.
