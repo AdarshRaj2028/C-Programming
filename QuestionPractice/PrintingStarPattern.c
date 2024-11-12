57e5ad92450248a1a80927bdf72789b6d33c2cd6
AdarshRaj <adarsh787011@gmail.com>
2024-11-12 17:51:23 +0530

    Added Functions for each pattern

--- QuestionPractice/PrintingStarPattern.c
+++ QuestionPractice/PrintingStarPattern.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+
+void func1(int z){
+    for (int i = 0; i < z; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        } 
+}
+
+void func2(int z){
+    for (int i = 0; i < z; i++)
+        {
+            for (int j = i; j < z; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+}
+
+void func3(int z){
+              for (int i = 0; i < z; i++) {
+  
+        for (int j = 0; j < z - i - 1; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
     int user_input, input;
@@ -12,42 +48,19 @@ int main()
     case 1:
         printf("Enter the number of rows: ");
         scanf("%d", &input);
-         for (int i = 0; i < input; i++)
-        {
-            for (int j = 0; j <= i; j++)
-            {
-                printf("*");
-            }
-            printf("\n");
-        } 
+        func1(input);
         break;
         
     case 2:
         printf("Enter the number of rows: ");
         scanf("%d", &input);
-        for (int i = 0; i < input; i++)
-        {
-            for (int j = i; j < input; j++)
-            {
-                printf("*");
-            }
-            printf("\n");
-        }
+     func2(input);
     break;
     
     case 3:
         printf("Enter the number of rows: ");
         scanf("%d", &input);
-        for (int i = 0; i < input; i++) {
-  
-        for (int j = 0; j < input - i - 1; j++) {
-            printf(" ");
-        }
-        for (int k = 0; k < 2 * i + 1; k++) {
-            printf("*");
-        }
-        printf("\n");
-    }  
+     func3(input);
     break;
     
     default:
