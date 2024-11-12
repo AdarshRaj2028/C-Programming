560f0c56ad166e2a471e10bec46ab19062de37b4
AdarshRaj <adarsh787011@gmail.com>
2024-11-12 13:44:57 +0530

    Added one more pyramid pattern.

--- QuestionPractice/PrintingStarPattern.c
+++ QuestionPractice/PrintingStarPattern.c
@@ -4,6 +4,7 @@ int main()
     int user_input, input;
     printf("Enter 1 to see Traingular star pattern\n");
     printf("Enter 2 to see Reversed Traingular star pattern\n");
+    printf("Enter 3 to see Pyramid pattern\n");
     printf("Enter your decision: ");
     scanf("%d", &user_input);
     switch (user_input)
@@ -20,6 +21,7 @@ int main()
             printf("\n");
         } 
         break;
+        
     case 2:
         printf("Enter the number of rows: ");
         scanf("%d", &input);
@@ -33,8 +35,23 @@ int main()
         }
     break;
     
+    case 3:
+        printf("Enter the number of rows: ");
+        scanf("%d", &input);
+        for (int i = 0; i < input; i++) {
+  
+        for (int j = 0; j < input - i - 1; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }  
+    break;
+    
     default:
-    printf("Please enter input as 1 or 2.");
+    printf("Please enter input as 1 or 2 or 3.");
         break;
     }
     return 0;
