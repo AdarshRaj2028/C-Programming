b22614ebcecb774240daf1847835f7a4f811b8a5
AdarshRaj <adarsh787011@gmail.com>
2024-11-14 15:26:28 +0530

    Understood and modified the program.

--- QuestionPractice/ArmstrongNumber.c
+++ QuestionPractice/ArmstrongNumber.c
@@ -1,93 +1,3 @@
-// #include <stdio.h>
-
-// int main() {
-// int i, n, x, sum = 0, sum1, temp, digit;
-// printf("Enter the number: ");
-// scanf("%d", &x);
-// sum1 = x;
-// temp = x;
-// while (temp > 0){
-//     temp--;
-    
-// }
-// while(x > 0){
-//     i = x % 10;
-//     sum += n;
-//     x = x / 10; 
-//     }
-// printf("%d\n", sum);
-// if(sum == sum1){
-//     printf("Yes");
-// }
-// else{
-//     printf("No");
-// }  
-//     return 0;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-// #include <math.h>
-
-// int main() {
-//     int x, original, sum = 0, digit, count = 0;
-
-//     printf("Enter the number: ");
-//     scanf("%d", &x);
-
-//     original = x;
-
-//     // Count the number of digits
-//     while (x > 0) {
-//         x /= 10;
-//         count++;
-//     }
-
-//     x = original; // Reset x to original value
-
-//     // Calculate the sum of each digit raised to the power of count
-//     while (x > 0) {
-//         digit = x % 10;
-//         sum += (int)pow(digit, count);
-//         x /= 10;
-//     }
-
-//     printf("Sum of digits raised to the power of number of digits: %d\n", sum);
-
-//     // Check if the sum is equal to the original number
-//     if (sum == original) {
-//         printf("Yes\n");
-//     } else {
-//         printf("No\n");
-//     }
-//     return 0;
-// }
 #include <stdio.h>
 
 int main() {
@@ -98,28 +8,25 @@ int main() {
 
     original = x;
 
-    // Count the number of digits
     while (x > 0) {
         x /= 10;
         count++;
     }
 
-    x = original; // Reset x to original value
+    x = original; 
 
-    // Calculate the sum of each digit raised to the power of count
     while (x > 0) {
         digit = x % 10;
-        int power = 1; // Initialize power to 1
+        int power = 1; 
         for (int i = 0; i < count; i++) {
-            power *= digit; // Calculate digit^count
+            power *= digit;
         }
-        sum += power; // Add to sum
+        sum += power; 
         x /= 10;
     }
 
     printf("Sum of digits raised to the power of number of digits: %d\n", sum);
 
-    // Check if the sum is equal to the original number
     if (sum == original) {
         printf("Yes\n");
     } else {
