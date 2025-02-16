e35a8eb27fd85f2990596caf63710bb823da18b4
AdarshRaj <adarsh787011@gmail.com>
2025-02-17 01:33:20 +0530

    Corrected Terenary operator code.

--- QuestionPractice/Max_of_four_num.c
+++ QuestionPractice/Max_of_four_num.c
@@ -12,12 +12,12 @@
 // return d;}
 // }
 
-int max_of_four(int a, int b, int c, int d){
-    (a > b && a > c && a > d) ?  a :  0;
-    (b > c && b > d) ?  b :  0; 
-    (c > d) ?  c :  0;
-    return d;
+int max_of_four(int a, int b, int c, int d) {
+    return (a > b) 
+        ? ((a > c) ? ((a > d) ? a : d) : ((c > d) ? c : d))
+        : ((b > c) ? ((b > d) ? b : d) : ((c > d) ? c : d));
 }
+
 
 int main(){
 int a, b, c, d, choice;
