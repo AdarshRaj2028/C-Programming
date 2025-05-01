edef0dee1dbd2515b5051b226b49fdbdb1d3b910
AdarshRaj <adarsh787011@gmail.com>
2025-05-01 15:34:02 +0530

    Added a program to find transpose of a matrix, and also to reverse the columns.

---
+++ QuestionPractice/transpose_mat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    int rows, columns;
+    printf("Enter number of rows : ");
+    scanf("%d", &rows);
+    printf("Enter number of columns : ");
+    scanf("%d", &columns);
+    int matrix[rows][columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("Enter the elements: ");
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    // FOR DISPLAYING THE MATRIX
+    printf("\nThe required matrix is:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    // TO FIND THE TRANSPOSE OF MATRIX
+    printf("\nThe transpose of matrix is:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d\t", matrix[j][i]);
+        }
+        printf("\n");
+    }
+
+    // FOR REVERSING THE MATRIX
+    printf("\nThe reverse of matrix is:\n");
+    for (int i = rows - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
